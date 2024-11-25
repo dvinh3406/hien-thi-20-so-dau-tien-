bd72073bb1bdeab2359fa716e9c0eb80eb867002
dvinh3406 <pdv206@gmail.com>
2024-11-25 15:34:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+   int n, demuoc, demnguyento
+    n=2;
+    demnguyento=0;
+    do
+    {
+    demuoc=0;
+    for(int i=1;i<=n,i++)
+        if(n%i==0)
+           demuoc++;
+    if(demuoc==2)
+    {
+        cout<<n<<" ";
+        demnguyento++;
+    }   
+    n++;
+  }while(demnguyento<20);
+  return 0;
+} 
